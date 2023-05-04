5d7cf34021af87ae69ea9c651f22079712f2dcfd
Arnold Burnley <aburnleylvx@outlook.com>
2023-05-04 08:15:28 -0400

    feat: added another main to return dynamic val from site

--- web-automation/SimpleTextScrapeWeb.py
+++ web-automation/SimpleTextScrapeWeb.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import time as t
 
 service = Service('C:\\Users\\aburn\\Downloads\\chromedriver_win32\\chromedriver.exe')
 
@@ -19,11 +20,27 @@ def initiate_driver():
 
     return driver
 
+# def main():
+#     """create instance of driver and scrape text value from given website"""
+#     driver = initiate_driver()
+#     element = driver.find_element(by = "xpath", value = "/html/body/div[1]/div/h1[1]")
+#     return element.text
+
+def get_temperature_from_text(text):
+    """ INPUT: str input formated like sdjndjlf jfl: ___ where the blank
+        value is a changing number
+        OUTPUT: the number as a float
+    """
+    output = float(text.split(": ")[1])
+    return output
+
 def main():
-    """create instance of driver and scrape text value from given website"""
+    """createe instance of driver and scrape a dynamic value"""
     driver = initiate_driver()
-    element = driver.find_element(by = "xpath", value = "/html/body/div[1]/div/h1[1]")
-    return element.text
+    # pause script to allow dynamic value to load
+    t.sleep(2)
+    element = driver.find_element(by = "xpath", value = "/html/body/div[1]/div/h1[2]")
+    return get_temperature_from_text(element.text)
 
 if __name__ == "__main__":
     print(main())
